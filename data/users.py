988b4be5f3d9c781b49200f89c865e9c265b06f9
Артем <makarenkoiandeks@yandex.ru>
2021-04-20 16:15:21 +0400

    v1.98

--- data/users.py
+++ data/users.py
@@ -7,7 +7,7 @@ from flask_login import  UserMixin
 class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True)
     nickname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     city_from = sqlalchemy.Column(sqlalchemy.String, nullable=True)
