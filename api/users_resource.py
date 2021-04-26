023a7a598084ae02916b47a20c65aa636634f656
Артем <makarenkoiandeks@yandex.ru>
2021-04-26 18:27:44 +0400

    v4.2(API Users, post, delete)

--- api/users_resource.py
+++ api/users_resource.py
@@ -1,12 +1,11 @@
-from flask_restful import Resource, abort, url_for
+from flask_restful import Resource, abort, url_for, reqparse
 from flask import jsonify
 
 from data import db_session
 from data.users import User
-from fuzzywuzzy import fuzz
 
 
-def abort_if_lessons_not_found(user_id):
+def abort_if_users_not_found(user_id):
     session = db_session.create_session()
     user = session.query(User).get(user_id)
     if not user:
@@ -18,14 +17,30 @@ def abort_if_lessons_not_found(user_id):
 
 class UsersResource(Resource):
     def get(self, user_id):
-        user = [abort_if_lessons_not_found(user_id)]
+        user = [abort_if_users_not_found(user_id)]
         users = []
         for i in user:
             i.image = i.image.hex()
             users.append(i)
         return jsonify({'user': {user.id: user.to_dict(
             only=[i for i in list(vars(user).keys()) if i not in ['_sa_instance_state', 'id']]) for
-                                    user in users}})
+            user in users}})
+
+    def delete(self, user_id):
+        news = abort_if_users_not_found(user_id)
+        session = db_session.create_session()
+        session.delete(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+parser = reqparse.RequestParser()
+parser.add_argument('nickname', required=True)
+parser.add_argument('password', required=True)
+parser.add_argument('password_again', required=True)
+parser.add_argument('city_from', required=True)
+parser.add_argument('image', required=True)
+parser.add_argument('email', required=True)
+parser.add_argument('about', required=True)
 
 class UsersListResource(Resource):
     def get(self):
@@ -37,4 +52,20 @@ class UsersListResource(Resource):
             users.append(i)
         return jsonify({'users': {user.id: user.to_dict(
             only=[i for i in list(vars(user).keys()) if i not in ['_sa_instance_state', 'id']]) for
-                                    user in users}})
+            user in users}})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        if args['password'] != args['password_again']:
+            return jsonify({'password': 'Password mismatch'})
+        user = User(
+            nickname=args['nickname'],
+            city_from=args['city_from'],
+            image=args['image'],
+            email=args['email'],
+            about=args['about'])
+        user.set_password(args['password'])
+        session.add(user)
+        session.commit()
+        return jsonify({'success': 'OK'})
