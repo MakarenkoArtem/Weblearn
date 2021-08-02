6c746cf1597552450d968bf46d72d5924510a001
artem <makarenkoiandeks@yandex.ru>
2021-08-02 16:34:42 +0400

    v4.6(localhost)

--- data/users.py
+++ data/users.py
@@ -4,6 +4,7 @@ from .db_session import SqlAlchemyBase
 from flask_login import UserMixin
 from sqlalchemy_serializer import SerializerMixin
 
+
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,30 @@
-let switchMode =  document.getElementById("switchMode")
-switchMode.onclick = function (){
+let switchMode = document.getElementById("switchMode")
+switchMode.onclick = function () {
     let theme = document.getElementById("theme")
-    if (theme.getAttribute("href") == "/static/css/dark.css"){
+    if (theme.getAttribute("href") == "/static/css/dark.css") {
         theme.href = "/static/css/light.css"
     } else {
         theme.href = "/static/css/dark.css"
     }
 }
-function test (){
+
+function test() {
     let theme = document.getElementById("theme")
-    if (theme.getAttribute("href") == "/static/css/dark.css"){
+    console.log(localStorage.getItem("theme"))
+    if (theme.getAttribute("href") == "/static/css/dark.css") {
         theme.href = "/static/css/light.css"
+        localStorage.removeItem("theme")
     } else {
         theme.href = "/static/css/dark.css"
+        localStorage.setItem("theme", 'dark')
+    }
+}
+
+function init() {
+    let theme = document.getElementById("theme")
+    console.log(localStorage.getItem("theme"))
+    if (localStorage.getItem("theme") !== null) {
+        theme.href = "/static/css/dark.css"
+        localStorage.setItem("theme", 'dark')
     }
 }
