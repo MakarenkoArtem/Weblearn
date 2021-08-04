d2e47b7d19b08024a812470011ca870e3ab5d267
artem <makarenkoiandeks@yandex.ru>
2021-08-04 21:35:48 +0400

    v4.67

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,32 @@
-let switchMode = document.getElementById("switchMode")
+let switchMode = document.getElementById("switchMode");
 switchMode.onclick = function () {
-    let theme = document.getElementById("theme")
+    let theme = document.getElementById("theme");
     if (theme.getAttribute("href") == "/static/css/dark.css") {
-        theme.href = "/static/css/light.css"
+        theme.href = "/static/css/light.css";
     } else {
-        theme.href = "/static/css/dark.css"
+        theme.href = "/static/css/dark.css";
     }
 }
 
 function test() {
-    let theme = document.getElementById("theme")
-    console.log(localStorage.getItem("theme"))
-    /*alert("Error")     так можно вывдить сообщения*/
+    let theme = document.getElementById("theme");
+    /*alert("Error");     так можно выводить сообщения*/
     if (theme.getAttribute("href") == "/static/css/dark.css") {
-        theme.href = "/static/css/light.css"
-        localStorage.removeItem("theme")
+        theme.href = "/static/css/light.css";
+        localStorage.setItem("theme", "light");
     } else {
-        theme.href = "/static/css/dark.css"
-        localStorage.setItem("theme", 'dark')
+        theme.href = "/static/css/dark.css";
+        localStorage.setItem("theme", 'dark');
     }
+    console.log(`New theme: ${localStorage.getItem("theme")}`);
 }
 
 function init() {
-    let theme = document.getElementById("theme")
-    console.log(localStorage.getItem("theme"))
-    if (localStorage.getItem("theme") !== null) {
-        theme.href = "/static/css/dark.css"
-        console.log(localStorage.setItem("theme"))
-        localStorage.setItem("theme", 'dark')
+    let theme = document.getElementById("theme");
+    console.log(`Theme: ${localStorage.getItem("theme")}`);
+    if (localStorage.getItem("theme") === "dark") {
+        theme.href = "/static/css/dark.css";
+        console.log(localStorage.setItem("theme"));
+        localStorage.setItem("theme", 'dark');
     }
 }
